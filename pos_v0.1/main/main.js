d5694b9365a2436984da46d0f4c9477eb5974d93
wangjunjun19 <735276168@qq.com>
2016-04-19 18:29:49 +0800

    first commit

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -2,3 +2,22 @@
 function printReceipt(inputs) {
 
 }
+function buildIteams(inputs) {
+  var Iteams=[{iteam:inputs[0],count:1}];
+  for(var i=1;i<inputs.length;i++) {
+    for(var j=0;j<Iteams.length;j++) {
+      if( Iteams[j].item.barcode===inputs[i].barcode) {
+        Iteams[j].count++;
+        break;
+      }
+    }
+    if(j===Iteams.length) {
+      Iteams.length++;
+      Iteams.push({iteam:inputs[i],count:1});
+    }
+  }
+  return Iteams;
+}
+function bulidCartIteams() {
+  
+}
